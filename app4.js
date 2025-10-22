63e7fe2b5e45b7fcc3d22851765833970cbdc27e
Khursheed <ka.pixelbits@gmail.com>
2025-10-22 15:54:39 +0500

    add process.argv to use user based inputs for place name and units.

--- app4.js
+++ app4.js
@@ -1,9 +1,23 @@
 import app3 from './app3.js';
 
-app3.getWeatherData('hawaii', 'f', (error, data) => {
-  if (error) {
-    console.log('Error: ', error);
-  } else {
-    console.log('Data: ', data);
-  }
-});
+console.log(process.argv);
+
+const query = process.argv[2];
+let units = process.argv[3];
+
+if (!units) {
+  units = 'm';
+}
+
+if (!query) {
+  console.log('Please enter place name!');
+  process.exit(1);
+} else {
+  app3.getWeatherData(query, units, (error, data) => {
+    if (error) {
+      console.log('Error: ', error);
+    } else {
+      console.log('Data: ', data);
+    }
+  });
+}
